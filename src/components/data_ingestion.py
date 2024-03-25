f1b61d5b61f08d53cc30d1fc2b46893ba9f991bd
niazroky <niazroky75@gmail.com>
2024-03-26 01:22:32 +0600

    Model Trainer and HyperParameter Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,8 +11,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 
@@ -93,10 +93,13 @@ class DataIngestion:
 
 
 if __name__ == "__main__":
+    
     # Create instance of DataIngestion class and initiate data ingestion process
     obj=DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
 
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,8 @@
 import os
 import sys
-
 import numpy as np
 import pandas as pd
+
 import dill
 import pickle
 
@@ -12,8 +12,18 @@ from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 
-def save_object(file_path, obj):
 
+def save_object(file_path, obj):
+    """
+    Saves a Python object to a file using pickle serialization.
+
+    Args:
+        file_path (str): Path to the file where the object will be saved.
+        obj: Python object to be saved.
+
+    Raises:
+        CustomException: If an error occurs during object serialization or file writing.
+    """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
@@ -23,12 +33,30 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+
 
 def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    Evaluates machine learning models using grid search and reports performance metrics.
+
+    Args:
+        X_train (array-like): Feature matrix of the training data.
+        y_train (array-like): Target values of the training data.
+        X_test (array-like): Feature matrix of the testing data.
+        y_test (array-like): Target values of the testing data.
+        models (dict): Dictionary containing machine learning models to be evaluated.
+        param (dict): Dictionary containing hyperparameters for each model.
+
+    Returns:
+        dict: A dictionary containing the performance scores of each model on the testing data.
+
+    Raises:
+        CustomException: If an error occurs during model evaluation.
+    """
     try:
         report = {}
-        
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
             para = param[list(models.keys())[i]]
@@ -48,14 +76,25 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
     except Exception as e:
         raise CustomException(e, sys)
-    
+
 
 
 def load_object(file_path):
+    """
+    Loads a Python object from a file.
 
+    Args:
+        file_path (str): Path to the file containing the object to be loaded.
+
+    Returns:
+        obj: The Python object loaded from the file.
+
+    Raises:
+        CustomException: If an error occurs during object deserialization or file reading.
+    """
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
